ced9c256a598cc4d70fe08039ee7e422b5c772d7
Ranjith679 <ranjithakm679@gmail.com>
2026-07-04 11:48:28 +0530

    initial set up of project

---
+++ src/test/java/com/secureauth/secure_auth_service/SecureAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureauth.secure_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
